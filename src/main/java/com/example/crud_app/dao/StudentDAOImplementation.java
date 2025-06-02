9d2047c3aca65818821812a67224ae2c171b5906
Sandu Guzun <sanduguzun762@gmail.com>
2025-06-02 17:48:41 +0300

    add somehintg

---
+++ src/main/java/com/example/crud_app/dao/StudentDAOImplementation.java
@@ -0,0 +1,34 @@
+package com.example.crud_app.dao;
+
+import com.example.crud_app.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImplementation implements StudentDAO {
+
+    //Camp pentru EntiryManager va fi utilizat pentru interactiunea
+    //cu baza de date
+    private EntityManager entityManager;
+
+    // Injectarea EntiryManager prin constructor (practica recomandata
+    //pentru testabiliatetea si modulariattea
+    @Autowired
+    public StudentDAOImplementation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    //Implementarea metodei save pentru a salvarea unui obiect
+    //student in baza de date 40000
+
+    //idk
+    int name = 20;
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+
+    }
+}
