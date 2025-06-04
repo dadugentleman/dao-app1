8365bd837e1c72b4b06f57890365be84615a77e4
Sandu Guzun <sanduguzun762@gmail.com>
2025-06-04 19:55:38 +0300

    Chore: add read method

--- src/main/java/com/example/crud_app/CrudAppApplication.java
+++ src/main/java/com/example/crud_app/CrudAppApplication.java
@@ -19,7 +19,9 @@ public class CrudAppApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner ->{
-			createStudent(studentDAO);
+			//createStudent(studentDAO);
+			//createMultipleStudents(studentDAO);
+			readStudent(studentDAO);
 		};
 	}
 
@@ -42,14 +44,38 @@ public class CrudAppApplication {
 		//creeam mai multi studenti
 		System.out.println("Creating 3 student objects");
 		Student newStudent1 = new Student("Andrei", "Munteanu", "andrei@gmail.com");
+
 		Student newStudent2 = new Student("Iulian", "Vatamanu", "iulic@gmail.com");
+
 		Student newStudent3 = new Student("Maria", "Mirabel", "maria@gmail.com");
+
 
 		//salvam obiectele student in baza de date
 		System.out.println("Saving the students");
 		studentDAO.save(newStudent1);
 		studentDAO.save(newStudent2);
 		studentDAO.save(newStudent3);
+
+	}
+	private void readStudent(StudentDAO studentDAO) {
+		// creeaza un obiect de tip Student
+		System.out.println("Creating new student object...");
+		Student newStudent = new Student("mircea", "Popescu", "mircea@gmail.com");
+
+		// salveaza studentul in baza de date
+		System.out.println("Saving the student");
+		studentDAO.save(newStudent);
+
+		// afiseaza id-ul studentului salvat
+		int theId = newStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+
+		// recupereaza studentul pe baza id-ului
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// afiseaza detaliile studentului
+		System.out.println("Found the student: " + myStudent);
 	}
 
 }
--- src/main/java/com/example/crud_app/dao/StudentDAOImplementation.java
+++ src/main/java/com/example/crud_app/dao/StudentDAOImplementation.java
@@ -29,4 +29,8 @@ public class StudentDAOImplementation implements StudentDAO {
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
     }
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
 }
